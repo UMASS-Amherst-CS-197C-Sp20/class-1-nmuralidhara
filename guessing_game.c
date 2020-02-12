79cfbfb1ac510a5a75e7c8e6f29f066414714cfa
Narpavirajan Venkatesan Muralidharan <nmuralidhara@umass.edu>
2020-02-12 17:08:42 -0500

    Add files via upload

--- guessing_game.c
+++ guessing_game.c
@@ -1,37 +1,51 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int main(void) {
-	/*
-		TODO:
-		change this guessing game to make it accept command line arguement
-		or accept user input so that it can generate a random secret number
-		within user specified range.
-		Google rand() and srand()
-	*/
-	//////////Your code here//////////
-	int secret = 77;
-
-	//////////End of the code/////////
-
-	printf("Welcome to my guessing game!\n");
-	while(true) {
-		// Print a prompt (part of a line)
-		printf("Type your guess and then ENTER: ");
-		// Make sure it becomes visible (otherwise C is allowed to wait
-		// until you print a new line, e.g., '\n' before doing anything.
-		fflush(stdout); // https://www.geeksforgeeks.org/use-fflushstdin-c/
-		int guess = 0;
-		int status = scanf("%d", &guess);
-		if (status != 1) break;
-			printf("You guessed: %d\n", guess);
-		if (secret == guess) {
-			printf("Congratulations! You guessed the secret.\n");
-			break;
-		} else if (secret < guess) {
-			printf("Too high! Guess again.\n");
-		} else {
-			printf("Too low! Guess again.\n");
-		}
-	}
-}
+// #include <stdio.h>
+// #include <stdbool.h>
+// #include <stdio.h> 
+// #include <stdlib.h> 
+// #include <time.h> 
+// int main(void) {
+// 	/*
+// 		TODO:
+// 		change this guessing game to make it accept command line arguement
+// 		or accept user input so that it can generate a random secret number
+// 		within user specified range.
+// 		Google rand() and srand()
+// 	*/
+// 	//////////Your code here//////////
+// 	int secret = 77;
+//   printf("Type your upper bound and then ENTER: ");
+//    fflush(stdout);
+//   int upperBound = 0;
+// 	int status1 = scanf("%d", &upperBound);
+//   // printf("Type your lower bound and then ENTER: ");
+//   //  fflush(stdout);
+//   // int lowerBound = 0;
+// 	// int status2 = scanf("%d", &lowerBound);
+//   if(status1!=1){
+//     return 0;
+//   }
+//    int num = (rand() % 
+//            (upperBound + 1));
+// 	//////////End of the code/////////
+//   printf("The answer is %d\n",num);
+// 	printf("Welcome to my guessing game!\n");
+// 	while(true) {
+// 		// Print a prompt (part of a line)
+// 		printf("Type your guess and then ENTER: ");
+// 		// Make sure it becomes visible (otherwise C is allowed to wait
+// 		// until you print a new line, e.g., '\n' before doing anything.
+// 		fflush(stdout); // https://www.geeksforgeeks.org/use-fflushstdin-c/
+// 		int guess = 0;
+// 		int status = scanf("%d", &guess);
+// 		if (status != 1) break;
+// 			printf("You guessed: %d\n", guess);
+// 		if (secret == guess) {
+// 			printf("Congratulations! You guessed the secret.\n");
+// 			break;
+// 		} else if (secret < guess) {
+// 			printf("Too high! Guess again.\n");
+// 		} else {
+// 			printf("Too low! Guess again.\n");
+// 		}
+// 	}
+// }
--- inclass1.c
+++ inclass1.c
@@ -1,148 +1,171 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <float.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <limits.h>
+// #include <float.h>
 
-void func(int i);
-void func1(int* i);
+// void func(int i);
+// void func1(int* i);
+// void print_string(char* a);
+// int main(int argc, char** argv){
+// 	printf("---Lets see the size of the data types---\n");
+// 	/*
+// 		Sizeof is a much used operator in the C or C++.
+// 		It is a compile time unary operator which can be
+// 		used to compute the size of its operand.
+// 		The result of sizeof is of unsigned integral type
+// 		which is usually denoted by size_t. sizeof can be
+// 		applied to any data-type, including primitive types
+// 		such as integer and floating-point types, pointer types,
+// 		or compound datatypes such as Structure, union etc.
+// 	*/
+// 	printf("size of char: %lu\n", sizeof(char));
+// 	printf("size of short: %lu\n", sizeof(short));
+// 	printf("size of int: %lu\n", sizeof(int));
+// 	printf("size of long: %lu\n", sizeof(long));
+// 	printf("size of float: %lu\n", sizeof(float));
+// 	printf("size of double: %lu\n", sizeof(double));
 
-int main(int argc, char** argv){
-	printf("---Lets see the size of the data types---\n");
-	/*
-		Sizeof is a much used operator in the C or C++.
-		It is a compile time unary operator which can be
-		used to compute the size of its operand.
-		The result of sizeof is of unsigned integral type
-		which is usually denoted by size_t. sizeof can be
-		applied to any data-type, including primitive types
-		such as integer and floating-point types, pointer types,
-		or compound datatypes such as Structure, union etc.
-	*/
-	printf("size of char: %lu\n", sizeof(char));
-	printf("size of short: %lu\n", sizeof(short));
-	printf("size of int: %lu\n", sizeof(int));
-	printf("size of long: %lu\n", sizeof(long));
-	printf("size of float: %lu\n", sizeof(float));
-	printf("size of double: %lu\n", sizeof(double));
+// 	/*
+// 		TODO:
+// 		print the size of a type plus another type, see the example
+// 		int+long
+// 	*/
+// 	printf("#####student code start#####\n");
+// 	//////////Your code here//////////
 
-	/*
-		TODO:
-		print the size of a type plus another type, see the example
-		int+long
-	*/
-	printf("#####student code start#####\n");
-	//////////Your code here//////////
+// 	//example
+// 	int type_int = 0;
+// 	long type_long = 0;
+// 	printf("size of int+long: %lu\n", sizeof(type_int+type_long));
+//   short type_short = 0;
+// 	float type_float = 0.0;
+//   double type_double = 0.0;
+//   char type_char= 'c';
+// 	printf("size of short+float: %lu\n", sizeof(type_short+type_float));
+//   printf("size of int+short: %lu\n", sizeof(type_int+type_short));
+//   printf("size of int+float: %lu\n", sizeof(type_int+type_float));
+//   printf("size of long+float: %lu\n", sizeof(type_long+type_float));
+//   printf("size of long+double: %lu\n", sizeof(type_long+type_double));
+//   printf("size of int+double: %lu\n", sizeof(type_int+type_double));
+//   printf("size of short+double: %lu\n", sizeof(type_short+type_double));
+//   printf("size of long+float: %lu\n", sizeof(type_long+type_float));
+//    printf("size of char+float: %lu\n", sizeof(type_char+type_float));
+//    printf("size of char+int: %lu\n", sizeof(type_char+type_int));
 
-	//example
-	int type_int = 0;
-	long type_long = 0;
-	printf("size of int+long: %lu\n", sizeof(type_int+type_long));
+// 	//////////End of the code/////////
+// 	printf("#####student code end#####\n");
 
-	//////////End of the code/////////
-	printf("#####student code end#####\n");
+// 	printf("\nWith different size, unsigned, or unsigned. \n");
 
-	printf("\nWith different size, unsigned, or unsigned. \n");
+// 	printf("CHAR_BIT    :   %d\n", CHAR_BIT);
+//     printf("CHAR_MAX    :   %d\n", CHAR_MAX);
+//     printf("CHAR_MIN    :   %d\n", CHAR_MIN);
+//     printf("INT_MAX     :   %d\n", INT_MAX);
+//     printf("INT_MIN     :   %d\n", INT_MIN);
+//     printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
+//     printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
+//     printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
+//     printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
+//     printf("SHRT_MAX    :   %d\n", SHRT_MAX);
+//     printf("SHRT_MIN    :   %d\n", SHRT_MIN);
+//     printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
+//     printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
+//     printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
+//     printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
 
-	printf("CHAR_BIT    :   %d\n", CHAR_BIT);
-    printf("CHAR_MAX    :   %d\n", CHAR_MAX);
-    printf("CHAR_MIN    :   %d\n", CHAR_MIN);
-    printf("INT_MAX     :   %d\n", INT_MAX);
-    printf("INT_MIN     :   %d\n", INT_MIN);
-    printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
-    printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
-    printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
-    printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
-    printf("SHRT_MAX    :   %d\n", SHRT_MAX);
-    printf("SHRT_MIN    :   %d\n", SHRT_MIN);
-    printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
-    printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
-    printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
-    printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
+//     printf("\n");
 
-    printf("\n");
+//     printf("Storage size for float : %lu \n", sizeof(float));
+//     printf("FLT_MAX     :   %g\n", (float) FLT_MAX);
+//     printf("FLT_MIN     :   %g\n", (float) FLT_MIN);
+//     printf("-FLT_MAX    :   %g\n", (float) -FLT_MAX);
+//     printf("-FLT_MIN    :   %g\n", (float) -FLT_MIN);
+//     printf("DBL_MAX     :   %g\n", (double) DBL_MAX);
+//     printf("DBL_MIN     :   %g\n", (double) DBL_MIN);
+//     printf("-DBL_MAX     :  %g\n", (double) -DBL_MAX);
+//     printf("Precision value: %d\n", FLT_DIG );
 
-    printf("Storage size for float : %lu \n", sizeof(float));
-    printf("FLT_MAX     :   %g\n", (float) FLT_MAX);
-    printf("FLT_MIN     :   %g\n", (float) FLT_MIN);
-    printf("-FLT_MAX    :   %g\n", (float) -FLT_MAX);
-    printf("-FLT_MIN    :   %g\n", (float) -FLT_MIN);
-    printf("DBL_MAX     :   %g\n", (double) DBL_MAX);
-    printf("DBL_MIN     :   %g\n", (double) DBL_MIN);
-    printf("-DBL_MAX     :  %g\n", (double) -DBL_MAX);
-    printf("Precision value: %d\n", FLT_DIG );
+//     /*
+// 		TODO:
+// 		print the value of max value of signed int + 1
+// 		print the value of max value of unsigned int + 1
+// 		print the value of max value of signed long + 1
+// 		print the value of max value of float + 1
+// 		print the value of max value of float + 1000
+// 	*/
+// 	printf("#####student code start#####\n");
+//     //////////Your code here//////////
+//     printf("INT_MAX+1     :   %d\n", (INT_MAX)+1);
+//     printf("UINT_MAX+1     :   %d\n", (UINT_MAX)+1);
+//     printf("LONG_MAX+1     :   %ld\n", (LONG_MAX)+1);
+//     printf("FLT_MAX+1     :   %f\n", (FLT_MAX)+1);
+//     printf("FLT_MAX+1     :   %f\n", (FLT_MAX)+1000);
 
-    /*
-		TODO:
-		print the value of max value of signed int + 1
-		print the value of max value of unsigned int + 1
-		print the value of max value of signed long + 1
-		print the value of max value of float + 1
-		print the value of max value of float + 1000
-	*/
-	printf("#####student code start#####\n");
-    //////////Your code here//////////
+// 	//////////End of the code/////////
+// 	printf("#####student code end#####\n");
 
-	//////////End of the code/////////
-	printf("#####student code end#####\n");
+// 	printf("\n");
 
-	printf("\n");
+// 	printf("\n String's pointer size \n");
+// 	char* s1 = "asd";
+// 	printf("size of s1: %lu\n", sizeof(s1));
+// 	char* s2 = "asdasdasd";
+// 	printf("size of s2: %lu\n", sizeof(s2));
+// 	/*
+// 		TODO:
+// 		Why we are getting this result?
+//     This is because we are getting the size of the pointer which stores the string  as unsigned long which is always 8.
+// 	*/
 
-	printf("\n String's pointer size \n");
-	char* s1 = "asd";
-	printf("size of s1: %lu\n", sizeof(s1));
-	char* s2 = "asdasdasd";
-	printf("size of s2: %lu\n", sizeof(s2));
-	/*
-		TODO:
-		Why we are getting this result?
-	*/
+// 	printf("\nIs 1 equals to true? \n");
+// 	//1 is true, 0 is false
+// 	if(1)
+// 		printf("1==true\n");
+// 	else
+// 		printf("1!=true\n");
 
-	printf("\nIs 1 equals to true? \n");
-	//1 is true, 0 is false
-	if(1)
-		printf("1==true\n");
-	else
-		printf("1!=true\n");
+// 	int i = 0;
+// 	func(i);
+// 	printf("func: %d\n", i);
+// 	func1(&i);
+// 	printf("func1: %d\n", i);
 
-	int i = 0;
-	func(i);
-	printf("func: %d\n", i);
-	func1(&i);
-	printf("func1: %d\n", i);
+// 	printf("Program Name Is: %s",argv[0]);
+//     if(argc==1){
+//     	printf("\nNo Extra Command Line Argument Passed Other Than Program Name\n");
+//         // return 0;
+//     }
+//     if(argc>=2)
+//     {
+//         printf("\nNumber Of Arguments Passed: %d",argc);
+//         printf("\n----Following Are The Command Line Arguments Passed----");
+//         for(int j=0; j<argc; j++)
+//             printf("\nargv[%d]: %s",j,argv[j]);
+//     }
 
-	printf("Program Name Is: %s",argv[0]);
-    if(argc==1){
-    	printf("\nNo Extra Command Line Argument Passed Other Than Program Name\n");
-        return 0;
-    }
-    if(argc>=2)
-    {
-        printf("\nNumber Of Arguments Passed: %d",argc);
-        printf("\n----Following Are The Command Line Arguments Passed----");
-        for(int j=0; j<argc; j++)
-            printf("\nargv[%d]: %s",j,argv[j]);
-    }
+//     //TODO: uncommend below code after complete print_string fucntion
+//     print_string(s1);
+//     print_string(s2);
 
-    //TODO: uncommend below code after complete print_string fucntion
-    //print_string(s1);
-    //print_string(s2);
+//     //exit status for the OS, 0 means no error.
+//     //It is different than the C true/false value
+// 	return 0;
+// }
 
-    //exit status for the OS, 0 means no error.
-    //It is different than the C true/false value
-	return 0;
-}
+// void func(int i){
+// 	i++;
+// }
 
-void func(int i){
-	i++;
-}
+// void func1(int* i){
+// 	(*i)++;
+// }
 
-void func1(int* i){
-	(*i)++;
-}
-
-/*
-	TODO:
-	Add a function that name as "print_string", which takes a string
-	as input and no output.
-	This function needs to print the string in the concole.
-*/
+// /*
+// 	TODO:
+// 	Add a function that name as "print_string", which takes a string
+// 	as input and no output.
+// 	This function needs to print the string in the concole.
+// */
+// void print_string(char* a){
+//   printf("%s\n",a);
+// }
